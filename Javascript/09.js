3150c5f97479aee38dcc15e7c8fe6cc0e2b9d062
Develop <sergiohernandez.developer@gmail.com>
2024-09-25 13:49:01 +0200

    09.js Arrays o Arreglos

---
+++ Javascript/09.js
@@ -0,0 +1,7 @@
+//  Arrays o Arreglos
+
+const technologies = [20, 30, 40,  true, "React.js"] //Siempre es mejor tener Arrays de su misma familia, para no mezclar y que sea más limpio
+
+console.table(technologies)
+// Los Arrays siempre empiezan por el 0, el primer índex siempre es 0
+console.table(technologies[1])
